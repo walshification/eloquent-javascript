efa2822687f912e643e4491a651418774c5978d0
Christopher Walsh <walshification@gmail.com>
2017-04-23 17:04:55 -0500

    I dont know how this works

--- spec/chapter-04/aListSpec.js
+++ spec/chapter-04/aListSpec.js
@@ -3,15 +3,31 @@ describe('aList', function() {
     it('converts a given array to a linked list', function() {
       var expectedList = {
         value: 1,
-        next: {
+        rest: {
           value: 'foo',
-          next: {
+          rest: {
             value: 'bar',
-            next: null,
+            rest: null,
           },
         },
       };
       expect(arrayToList([1, 'foo', 'bar'])).toEqual(expectedList);
+    });
+  });
+
+  describe('listToArray', function() {
+    it('converts a given list to an array', function() {
+      converted = listToArray({
+        value: 1,
+        rest: {
+          value: 'foo',
+          rest: {
+            value: 'bar',
+            rest: null,
+          },
+        },
+      });
+      expect(converted).toEqual([1, 'foo', 'bar']);
     });
   });
 });
--- src/chapter-04/aList.js
+++ src/chapter-04/aList.js
@@ -7,11 +7,69 @@ function arrayToList(array) {
   if(array.length === 1) {
     return {
       value: array[0],
-      next: null,
+      rest: null,
     }
   };
   return {
     value: array[0],
-    next: arrayToList(array.slice(1)),
+    rest: arrayToList(array.slice(1)),
+  };
+}
+
+/**
+* Converts a given linked list to an array.
+* @param {object} list
+* @returns array
+*/
+function listToArray(list, array) {
+  var array = array || [];
+
+  array.push(recursiveNth(list, 0))
+
+  if(list.rest === null) {
+    return array;
+  } else {
+    return listToArray(list.rest, array);
+  }
+}
+
+/**
+* Given a list and element, returns a new list with the element at the front.
+* @param element
+* @param {object} list
+* @returns {object} prependedList
+*/
+function prepend(element, list) {
+  return {
+    value: element.value,
+    rest: list,
+  };
+}
+
+/**
+* Returns the element at a given position in a list or undefined if no such position.
+* @param {object} list
+* @param {number} number
+* @returns {object}
+*/
+function nth(list, number) {
+
+}
+
+/**
+* Returns the element at a given position in a list or undefined if no such position.
+* @param {object} list
+* @param {number} number
+* @returns {object}
+*/
+function recursiveNth(list, number, index) {
+  index = index || 0
+
+  if(index === number) {
+    return list.value;
+  } else if(list.rest === null) {
+    return undefined;
+  } else {
+    return recursiveNth(list.rest, number, index + 1);
   };
 }
